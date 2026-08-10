eaa3ae6637a8e272bcdd73c05ca71a48fbf832dd
Aviral Updahyay <aviral.pba322@webkul.in>
2026-08-10 16:43:37 +0530

    fic redirects

--- .vitepress/_redirects.ts
+++ .vitepress/_redirects.ts
@@ -111,7 +111,7 @@ export const redirects = {
   "/2.3.0/multi-vendor-marketplace/product-management.html":"https://marketplace-docs.bagisto.com/payments/payment-management.html",
   "/2.3.0/multi-vendor-marketplace/rating-management.html":"https://marketplace-docs.bagisto.com/moderation/rating-management",
   "/2.3.0/multi-vendor-marketplace/payment-management.html":"https://marketplace-docs.bagisto.com/payments/payment-management.html",
-  
+
 
   // B2B Marketplace
 
@@ -529,13 +529,13 @@ export const redirects = {
 
   // Multi Vendor Marketplace
 
-  "2.0/multi-vendor-marketplace/":"/multi-vendor-marketplace/vendor-management",
-  "/2.0/multi-vendor-marketplace/vendor-management.html":"/multi-vendor-marketplace/vendor-management",
-  "/2.0/multi-vendor-marketplace/order-management.html":"/multi-vendor-marketplace/order-management",
-  "/2.0/multi-vendor-marketplace/commission-management.html":"/multi-vendor-marketplace/commission-management",
-  "/2.0/multi-vendor-marketplace/product-management.html":"/multi-vendor-marketplace/product-management",
-  "/2.0/multi-vendor-marketplace/rating-management.html":"/multi-vendor-marketplace/rating-management",
-  "/2.0/multi-vendor-marketplace/payment-management.html":"/multi-vendor-marketplace/payment-management",
+  "2.0/multi-vendor-marketplace/":"https://marketplace-docs.bagisto.com/sellers/seller-management",
+  "/2.0/multi-vendor-marketplace/vendor-management.html":"https://marketplace-docs.bagisto.com/sellers/seller-management",
+  "/2.0/multi-vendor-marketplace/order-management.html":"https://marketplace-docs.bagisto.com/orders/order-management",
+  "/2.0/multi-vendor-marketplace/commission-management.html":"https://marketplace-docs.bagisto.com/payments/commission-management",
+  "/2.0/multi-vendor-marketplace/product-management.html":"https://marketplace-docs.bagisto.com/payments/payment-management",
+  "/2.0/multi-vendor-marketplace/rating-management.html":"https://marketplace-docs.bagisto.com/moderation/rating-management",
+  "/2.0/multi-vendor-marketplace/payment-management.html":"https://marketplace-docs.bagisto.com/payments/payment-management",
 
   // B2B Marketplace
 
